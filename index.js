5d008b9a1ad4bf0ae0e5cf9faca48c81f4a68eff
දේවමුණි සසිත අසරංග (DSA) <sasithaasaranga@gmail.com>
2019-11-12 13:20:13 +0530

    changed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 const app = express();
 const http = require('http');
-const fs = require('fs');
 
 app.get("/index/hello", (req, res) => {
     res.send("Hello world");
@@ -19,7 +18,7 @@ app.use(async function (req, res, next) {
 const server = http.createServer(app);
 server.listen(3000);
 server.on('error', (e) => {
-    fs.writeFile('./out.log', JSON.stringify(e), 'utf-8');
+    // fs.writeFile('./out.log', JSON.stringify(e), 'utf-8');
 });
 
 // curl --location --request POST "http://localhost:3000/index/getUsers" --data "" ;
